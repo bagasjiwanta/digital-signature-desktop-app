f59f7c12d552de50782f060b227744fb550ded16
pavita1108 <18220014@std.stei.itb.ac.id>
2023-04-13 20:19:11 +0700

    ui ux txt kelar

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QFileDialog, QApplication, QWidget, QMainWindow
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QPixmap
 from lib.rsa import generate_rsa, PublicKey, PrivateKey
 from lib.signing import sign_text_file, verify_text_file
-from lib.exception import FileModified, SignatureCorrupted, SignatureNotFound
+from lib.exception import *
 
 def cek_flow_rsa():
     try:
@@ -19,6 +19,7 @@ def cek_flow_rsa():
         # baca key
         private_key = PrivateKey.read_from_file('test/private_key.pri')
         public_key = PublicKey.read_from_file('test/public_key.pub')
+        print(public_key)
 
         sign_text_file('test/message.txt', 'test/output.txt', public_key)
         is_verified = verify_text_file('test/output.txt', private_key)
@@ -36,12 +37,22 @@ class Menu(QMainWindow):
         super(Menu, self).__init__()
         loadUi("ui/main_menu.ui", self)
         self.b_rsa.clicked.connect(self.RSA)
-        # self.DigitalSign.clicked.connect(self.Extended)
-        # self.Verif.clicked.connect(self.Playfair)
+        self.b_sign.clicked.connect(self.Sign)
+        self.b_verif.clicked.connect(self.Verify)
         
     def RSA(self):
             rsa = RSA()
             widget.addWidget(rsa)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def Sign(self):
+            sign = Sign()
+            widget.addWidget(sign)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+    
+    def Verify(self):
+            verify = Verify()
+            widget.addWidget(verify)
             widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
@@ -63,6 +74,84 @@ class RSA(QMainWindow):
         widget.addWidget(menu)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
+class Sign (QMainWindow):
+    fileLoc = ''
+    pubKey = ''
+    def __init__(self):
+        super(Sign, self).__init__()
+        loadUi("ui/digital_sign.ui", self)
+        self.importFileButton.clicked.connect(self.FileImport)
+        self.impPubKeyButton.clicked.connect(self.PubKeyImport)
+        self.generateSign.clicked.connect(self.GenerateSign)
+        self.backButton.clicked.connect(self.Menu)
+
+    def FileImport(self):
+        fname = QFileDialog.getOpenFileName(self, "Choose File", "")
+        self.fileLoc = fname[0]
+        self.label_3.setText("File Imported Successfully")
+
+    def PubKeyImport(self):
+        try : 
+            fname = QFileDialog.getOpenFileName(self, "Choose File", "")
+            self.pubKey = PublicKey.read_from_file(fname[0])
+            self.label_3.setText("Public Key Imported Successfully")
+        except CorruptedPublicKeyFile as e:
+            self.label_3.setText(e)
+
+    def GenerateSign (self):
+        loc = self.lineEdit.text()
+        sign_text_file(self.fileLoc, loc +'\output.txt', self.pubKey)
+        self.label_3.setText("Generated Successfully")
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class Verify (QMainWindow):
+    fileLoc = ''
+    privKey = ''
+    def __init__(self):
+        super(Verify, self).__init__()
+        loadUi("ui/verif.ui", self)
+        self.importFileButton.clicked.connect(self.FileImport)
+        self.impPrivKeyButton.clicked.connect(self.PrivKeyImport)
+        self.verifButton.clicked.connect(self.Verification)
+        self.backButton.clicked.connect(self.Menu)
+
+    def FileImport(self):
+        fname = QFileDialog.getOpenFileName(self, "Choose File", "")
+        self.fileLoc = fname[0]
+        self.label_3.setText("File Imported Successfully")
+    
+    def PrivKeyImport(self):
+        try : 
+            fname = QFileDialog.getOpenFileName(self, "Choose File", "")
+            self.privKey = PrivateKey.read_from_file(fname[0])
+            self.label_3.setText("Public Key Imported Successfully")
+        except CorruptedPrivateKeyFile as e:
+            self.label_3.setText(e)
+    
+    def Verification(self):
+        try:
+            is_verified = verify_text_file(self.fileLoc, self.privKey)
+            if (is_verified):
+                self.label_3.setText('Verified')
+        except FileModified as e:
+            print(e)
+            self.label_3.setText(e)
+        except SignatureNotFound as e:
+            print(e)
+            self.label_3.setText(e)
+        except SignatureCorrupted as e:
+            print(e)
+            self.label_3.setText(e)
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
 
 app = QApplication(sys.argv)
 welcome = Menu()
@@ -73,3 +162,4 @@ widget.setFixedWidth(900)
 widget.show()
 
 app.exec()
+
