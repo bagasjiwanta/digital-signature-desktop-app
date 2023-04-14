661be8a6603060fde642f9eddfdcad94ab281269
pavita1108 <18220014@std.stei.itb.ac.id>
2023-04-14 16:11:55 +0700

    Ui Ux final integration

--- lib/signing.py
+++ lib/signing.py
@@ -4,33 +4,33 @@ from lib.exception import SignatureNotFound, SignatureCorrupted, FileModified
 
 BUFFER_SIZE = 65536
 
-def sign_binary_file(input_file_name: str, key_file_name: str, public_key: PublicKey):
+def sign_binary_file(input_file_name: str, key_file_name: str, public_key: PrivateKey):
     '''menambahkan digital signature dari file binary ke file text'''
     with open(input_file_name, "rb") as f:
         content = f.read(BUFFER_SIZE)
     
     hash = int.from_bytes(SHA3_256.new(content).digest())
-    signature = public_key.encrypt(hash)
+    signature = public_key.decrypt(hash)
     output = "<ds>" + str(signature) + "</ds>\n"
 
     with open(key_file_name, "w") as f:
         f.write(output)
 
 
-def sign_text_file(input_file_name:str, output_file_name:str, public_key: PublicKey):
+def sign_text_file(input_file_name:str, output_file_name:str, public_key: PrivateKey):
     '''menambahkan digital signature ke file text'''
     with open(input_file_name, "r") as f:
         content = f.read()
 
     hash = int.from_bytes(SHA3_256.new(content.encode()).digest())
-    signature = public_key.encrypt(hash)
+    signature = public_key.decrypt(hash)
     output = content+ "\n<ds>" + str(signature) + "</ds>"
 
     with open(output_file_name, "w") as f:
         f.write(output)
     
 
-def verify_binary_file(input_file_name: str, key_file_name: str, private_key: PrivateKey):
+def verify_binary_file(input_file_name: str, key_file_name: str, private_key: PublicKey):
 
     with open(key_file_name, "r") as f:
         content = f.read()
@@ -50,13 +50,13 @@ def verify_binary_file(input_file_name: str, key_file_name: str, private_key: Pr
         content = f.read(BUFFER_SIZE)
 
     hash = int.from_bytes(SHA3_256.new(content).digest())
-    decrypted = private_key.decrypt(signature)
+    decrypted = private_key.encrypt(signature)
     if decrypted != hash:
         raise FileModified
     return True
 
 
-def verify_text_file(input_file_name: str, private_key: PrivateKey):
+def verify_text_file(input_file_name: str, private_key: PublicKey):
     '''memeriksa keasilan file text dengan menguji digital signaturenya'''
     with open(input_file_name, "r") as f:
         content = f.read()
@@ -75,7 +75,7 @@ def verify_text_file(input_file_name: str, private_key: PrivateKey):
     signature = int(content[start_ds:end_ds])
     real_content = content[:start_tag]
     hash = int.from_bytes(SHA3_256.new(real_content.encode()).digest())
-    decrypted = private_key.decrypt(signature)
+    decrypted = private_key.encrypt(signature)
     if decrypted != hash:
         raise FileModified
     return True
