d236d83c08c142c36a9d38969830e5080d3317ad
bagasjiwanta <bagasjiwanta@gmail.com>
2023-04-14 12:19:36 +0700

    binary file open

--- lib/signing.py
+++ lib/signing.py
@@ -2,6 +2,20 @@ from Crypto.Hash import SHA3_256
 from lib.rsa import PublicKey, PrivateKey
 from lib.exception import SignatureNotFound, SignatureCorrupted, FileModified
 
+BUFFER_SIZE = 65536
+
+def sign_binary_file(input_file_name: str, key_file_name: str, public_key: PublicKey):
+    '''menambahkan digital signature dari file binary ke file text'''
+    with open(input_file_name, "rb") as f:
+        content = f.read(BUFFER_SIZE)
+    
+    hash = int.from_bytes(SHA3_256.new(content).digest())
+    signature = public_key.encrypt(hash)
+    output = "<ds>" + str(signature) + "</ds>\n"
+
+    with open(key_file_name, "w") as f:
+        f.write(output)
+
 
 def sign_text_file(input_file_name:str, output_file_name:str, public_key: PublicKey):
     '''menambahkan digital signature ke file text'''
@@ -10,15 +24,15 @@ def sign_text_file(input_file_name:str, output_file_name:str, public_key: Public
 
     hash = int.from_bytes(SHA3_256.new(content.encode()).digest())
     signature = public_key.encrypt(hash)
-    output = "<ds>" + str(signature) + "</ds>\n" + content
+    output = content+ "\n<ds>" + str(signature) + "</ds>"
 
     with open(output_file_name, "w") as f:
         f.write(output)
     
 
-def verify_text_file(input_file_name: str, private_key: PrivateKey):
-    '''memeriksa keasilan file text dengan menguji digital signaturenya'''
-    with open(input_file_name, "r") as f:
+def verify_binary_file(input_file_name: str, key_file_name: str, private_key: PrivateKey):
+
+    with open(key_file_name, "r") as f:
         content = f.read()
     
     start_tag = content.find("<ds>")
@@ -30,10 +44,36 @@ def verify_text_file(input_file_name: str, private_key: PrivateKey):
         raise SignatureCorrupted
     
     start_ds = start_tag + len("<ds>")
-    end_tag = end_ds + len("</ds>\n")
+    signature = int(content[start_ds:end_ds])
+
+    with open(input_file_name, "rb") as f:
+        content = f.read(BUFFER_SIZE)
+
+    hash = int.from_bytes(SHA3_256.new(content).digest())
+    decrypted = private_key.decrypt(signature)
+    if decrypted != hash:
+        raise FileModified
+    return True
+
+
+def verify_text_file(input_file_name: str, private_key: PrivateKey):
+    '''memeriksa keasilan file text dengan menguji digital signaturenya'''
+    with open(input_file_name, "r") as f:
+        content = f.read()
+    
+    start_tag = content.find("\n<ds>")
+    if start_tag == -1:
+        raise SignatureNotFound
+    
+    end_ds = content.find("</ds>")
+    if end_ds == -1:
+        raise SignatureCorrupted
+    
+    start_ds = start_tag + len("\n<ds>")
+    end_tag = end_ds + len("</ds>")
 
     signature = int(content[start_ds:end_ds])
-    real_content = content[end_tag:]
+    real_content = content[:start_tag]
     hash = int.from_bytes(SHA3_256.new(real_content.encode()).digest())
     decrypted = private_key.decrypt(signature)
     if decrypted != hash:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QPixmap
 from lib.rsa import generate_rsa, PublicKey, PrivateKey
-from lib.signing import sign_text_file, verify_text_file
+from lib.signing import sign_text_file, verify_text_file, sign_binary_file, verify_binary_file
 from lib.exception import FileModified, SignatureCorrupted, SignatureNotFound
 
 def cek_flow_rsa():
@@ -20,9 +20,14 @@ def cek_flow_rsa():
         private_key = PrivateKey.read_from_file('test/private_key.pri')
         public_key = PublicKey.read_from_file('test/public_key.pub')
 
-        sign_text_file('test/message.txt', 'test/output.txt', public_key)
-        is_verified = verify_text_file('test/output.txt', private_key)
+        sign_text_file('test/message.txt', 'test/outputtext.txt', public_key)
+        is_verified = verify_text_file('test/outputtext.txt', private_key)
         print(is_verified)
+
+        sign_binary_file('test/message.mp4', 'test/outputbinary.txt', public_key)
+        is_verified = verify_binary_file('test/message.mp4', 'test/outputbinary.txt', private_key)
+        print(is_verified)
+
         
     except FileModified as e:
         print(e)
@@ -63,7 +68,7 @@ class RSA(QMainWindow):
         widget.addWidget(menu)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-
+cek_flow_rsa()
 app = QApplication(sys.argv)
 welcome = Menu()
 widget = QtWidgets.QStackedWidget()
